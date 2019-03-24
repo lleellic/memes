ee797f912fe6bf7e5193bde9118640429387f8bf
b0tank <48216006+b0tank@users.noreply.github.com>
2019-03-24 03:10:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 
   
-var TelegramBot = require('node-telegram-bot-api');
-var TelegramBot = require('node-telegram-bot-api');
+const TelegramBot = require('node-telegram-bot-api');
+const TelegramBot = require('node-telegram-bot-api');
 
 // Устанавливаем токен, который выдавал нам бот.
-var token = '783491645:AAGp_rUoPcyRDfgfGZzsXIzJ8HtXbpJThyk';
+const token = '783491645:AAGp_rUoPcyRDfgfGZzsXIzJ8HtXbpJThyk';
 // Включить опрос сервера
-var bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token, {polling: true});
 
 // Написать мне ... (/echo Hello World! - пришлет сообщение с этим приветствием.)
 bot.onText(//echo (.+)/, function (msg, match) {
-    var fromId = msg.from.id;
-    var resp = match[1];
+    let fromId = msg.from.id;
+    let resp = match[1];
     bot.sendMessage(fromId, resp);
 });
 
 // Простая команда без параметров.
 bot.on('message', function (msg) {
-    var chatId = msg.chat.id;
+    let chatId = msg.chat.id;
     // Фотография может быть: путь к файлу, поток(stream) или параметр file_id
-    var photo = 'http://pngimg.com/uploads/cat/cat_PNG50550.png';
+    let photo = 'http://pngimg.com/uploads/cat/cat_PNG50550.png';
     bot.sendPhoto(chatId, photo, {caption: 'Милые котята'});
 });
