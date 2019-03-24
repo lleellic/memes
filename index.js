651d213d13c10640b8f25cf334fca698431d29af
b0tank <48216006+b0tank@users.noreply.github.com>
2019-03-24 03:41:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Telegraf = require('telegraf')
-
-const bot = new Telegraf(process.env.783491645:AAGp_rUoPcyRDfgfGZzsXIzJ8HtXbpJThyk, {polling: true})
+const bot = new Telegraf("783491645:AAGp_rUoPcyRDfgfGZzsXIzJ8HtXbpJThyk");
 bot.start((ctx) => ctx.reply('Welcome!'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
