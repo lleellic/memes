bab15c233bf454754ed15ecae53dd572a5c77aab
b0tank <48216006+b0tank@users.noreply.github.com>
2019-03-26 12:37:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const keyboard = Markup.inlineKeyboard([
 
 const bot = new Telegraf("783491645:AAGp_rUoPcyRDfgfGZzsXIzJ8HtXbpJThyk");
 
-bot.on('message', (ctx) => ctx.telegram.sendCopy(ctx.from.id, ctx.message, Extra.markup(keyboard)))
+//bot.on('message', (ctx) => ctx.telegram.sendCopy(ctx.from.id, ctx.message, Extra.markup(keyboard)))
 bot.start((ctx) => ctx.reply('Привет! Я бот IT_camp и Profcamp версия 0.1! Рад познакомиться! Сейчас ты можешь узнать, как выглядит твой инвестор или ментор. Для этого просто пришли мне его имя. Будь уважителен - используй полное имя и заглавную первую букву.'))
 bot.help((ctx) => ctx.reply('Пришли мне стикер'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
