72bd73cd7e49440a97933557a14e5b35cf4cd5b3
b0tank <48216006+b0tank@users.noreply.github.com>
2019-03-26 12:45:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -33,5 +33,6 @@ bot.hears('джейк', (ctx) => ctx.replyWithAnimation('https://99px.ru/sstorag
 bot.hears('Привет',(ctx) => ctx.reply('Привет'))
 bot.hears('Покажи лучший сериал',(ctx) => ctx.replyWithAnimation('https://i.gifer.com/W8zq.gif'))
 bot.hears('i want to belive', (ctx) => ctx.replyWithPhoto('https://d2h1pu99sxkfvn.cloudfront.net/b0/10474470/422884304_fMSA87eogk/P7.jpg'))
-
+bot.hears("Покажи босса", (ctx) => ctx.replyWithPhoto('http://www.stickpng.com/assets/images/585e9740cb11b227491c34e5.png'))
+bot.hears("Покажи котика", (ctx) => ctx.replyWithPhoto("https://i.pinimg.com/474x/7c/45/9e/7c459e56fd8e93ee7b22ad7e7db94a54.jpg"))
 bot.launch()
