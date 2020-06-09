6247c042c2e7fa3b146dae0ef731647c92082785
lleellic <66576165+lleellic@users.noreply.github.com>
2020-06-09 11:26:36 +0300

    Update members.js

--- members.js
+++ members.js
@@ -1,4 +1,4 @@
 let members = [684519513, 687542018, 240481520, 757403329, 1178427119, 645483421, 721564120, 1045964979, 1074618502, 851957031, 612495949, 932348113, 687329153, 564553040, 691269383, 751498406, 957137055, 565160580, 584206250]
 let usernames = ['oscu1um','karenbrattt','gloomyXIII','Son_Of_Moon_69','bnnclvr','none','linaaaaaaaaaaaaaaaaaaaaaaaaaq','nicebanana','Akana_Unamori','hochy_rollov','GeniusIlya','Get_away3','gerpaS','iraasynytsiaa','StupidKaktus','FastK1ll','askk1899','lokteva_olesya','ekusok']
 
-module.exports = { members }
+module.exports = { members }cdf
