34e70bcbeb2e3742cc25b3000b78674426904917
lleellic <66576165+lleellic@users.noreply.github.com>
2020-06-09 05:40:50 +0300

    Update index01.js

--- index01.js
+++ index01.js
@@ -11,6 +11,6 @@ const bot = new Telegraf("1097903013:AAHqfjyVsppFchDSZj9S_gJ6-MrZlgvDHyQ");
 bot.start((ctx) => ctx.reply('Привет! Я бот для мышеловки! Рад познакомиться! Введи команду /help для ознакомления со списком моих команд!'))
 bot.help((ctx) => ctx.reply('Я бот, пока что я умею только прощать'))
 
-bot.hears('Меня', (ctx) => telegram.setChatAdministratorCustomTitle(chatId, userId, [extra]))
+bot.hears('Меня', (ctx) => telegram.setChatAdministratorCustomTitle(1241810361, 684519513, [Ботолюб]))
 
 bot.launch()
