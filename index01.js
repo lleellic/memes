4f0415fcee8de9f7541596fd3dcc631c7198acc2
lleellic <66576165+lleellic@users.noreply.github.com>
2020-06-09 14:36:30 +0300

    Update index01.js

--- index01.js
+++ index01.js
@@ -3,11 +3,14 @@ const Extra = require('telegraf/extra')
 const fs = require('fs')
 var offir = 0;
 const balance = new Array();
+for (var i = 0, i < 19, i++) {
+ balance[i]= 0;
+}
 
 
 
 var members = [684519513, 687542018, 240481520, 757403329, 1178427119, 645483421, 721564120, 1045964979, 1074618502, 851957031, 612495949, 932348113, 687329153, 564553040, 691269383, 751498406, 957137055, 565160580, 584206250]
-var usernames = [];
+const usernames =  new array();
 usernames[0] = 'oscu1um';
 usernames[1] = 'karenbrattt';
 usernames[2] = 'gloomyXIII';
