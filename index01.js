f6cd028b5c455cb0ca2cac3abf908ddc5de983ae
lleellic <66576165+lleellic@users.noreply.github.com>
2020-06-09 11:19:14 +0300

    Update index01.js

--- index01.js
+++ index01.js
@@ -3,7 +3,7 @@ const Extra = require('telegraf/extra')
 const fs = require('fs')
 const members = require('./members')
 const balances = require('./members')
-
+const offir = 0;
 
 
 
@@ -13,14 +13,37 @@ const balances = require('./members')
 const bot = new Telegraf("1097903013:AAHqfjyVsppFchDSZj9S_gJ6-MrZlgvDHyQ");
 
 
-
+bot.hears('Выключить извинения', (ctx) => {
+          if (ctx.from.id == 684519513 || ctx.from.id == 687542018) {
+ if (offir == 1) ctx.reply('Извинения уже отключены')
+ else {
+        offir = 1;
+        ctx.reply('Извинения отключены')  
+  }
+ }
+})
+          
+bot.hears('Включить извинения', (ctx) => {
+          if (ctx.from.id == 684519513 || ctx.from.id == 687542018) {
+ if (offir == 0) ctx.reply('Извинения уже включены')
+ else {
+        offir = 0;
+        ctx.reply('Извинения включены')  
+  }
+ }
+})
 
 bot.start((ctx) => ctx.reply('Привет @'+ctx.from.username+'! Я бот для мышеловки! Рад познакомиться! Введи команду /help для ознакомления со списком моих команд!'))
 bot.help((ctx) => ctx.reply('@'+ctx.from.username+', я пока ничего не умею'))
 
 
-bot.hears(/прости/gi, (ctx) => ctx.reply('Хорошо, и ты меня🥺'))
-bot.hears(/извин/gi, (ctx) => ctx.reply('Прости меня🥺'))
+bot.hears(/прости/gi, (ctx) => {
+          if (offir == 0) ctx.reply('Хорошо, и ты меня🥺')
+})
+bot.hears(/извин/gi, (ctx) => {
+          if (offir == 0) ctx.reply('Прости меня🥺')
+})
+
 bot.command('shipper', (ctx) => { 
           var sh1 = Math.floor(Math.random() * 20) + 1;
           var sh2 = Math.floor(Math.random() * 10) + 1;
@@ -29,7 +52,6 @@ bot.command('shipper', (ctx) => {
 }
 if (sh1 < 10) sh1 = '0'+sh1;
 if (sh2 < 10) sh2 = '0'+sh1;
-            
           ctx.reply('Пара дня: @' +        
             
  })
