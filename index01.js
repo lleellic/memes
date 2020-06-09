2db82f2334dff70e8d58a9df3adb582a00a25685
lleellic <66576165+lleellic@users.noreply.github.com>
2020-06-09 10:16:55 +0300

    Update index01.js

--- index01.js
+++ index01.js
@@ -2,19 +2,65 @@ const Telegraf = require('telegraf')
 const Extra = require('telegraf/extra')
 const fs = require('fs')
 
-  
+
+const member_01 = 684519513;
+const member_02 = 687542018;
+const member_03 = 240481520;
+const member_04 = 757403329;
+const member_06 = 645483421;
+const member_07 = 721564120;
+const member_08 = 1045964979;
+const member_09 = 1074618502;
+const member_10 = 851957031;
+const member_11 = 612495949;
+const member_12 = 932348113;
+const member_13 = 687329153;
+const member_14 = 564553040;
+const member_15 = 691269383;
+const member_16 = 751498406;
+const member_17 = 957137055;
+const member_18 = 565160580;
+const member_19 = 910787068;
+const member_20 = 584206250;
+
+const balance_01;
+const balance_02;
+const balance_03;
+const balance_04;
+const balance_06;
+const balance_07;
+const balance_08;
+const balance_09;
+const balance_10;
+const balance_11;
+const balance_12;
+const balance_13;
+const balance_14;
+const balance_15;
+const balance_16;
+const balance_17;
+const balance_18;
+const balance_19;
+const balance_20;
+
+
 const bot = new Telegraf("1097903013:AAHqfjyVsppFchDSZj9S_gJ6-MrZlgvDHyQ");
 
 
 
 
-bot.start((ctx) => ctx.reply('Привет <a href="tg://user?id=${ctx.from.id}">${ctx.from.first_name}</a>! Я бот для мышеловки! Рад познакомиться! Введи команду /help для ознакомления со списком моих команд!'))
+bot.start((ctx) => ctx.reply('Привет @'+ctx.from.username+'! Я бот для мышеловки! Рад познакомиться! Введи команду /help для ознакомления со списком моих команд!'))
 bot.help((ctx) => ctx.reply('@'+ctx.from.username+', я пока ничего не умею'))
 
 
 bot.hears(/прости/gi, (ctx) => ctx.reply('Хорошо, и ты меня🥺'))
 bot.hears(/извин/gi, (ctx) => ctx.reply('Прости меня🥺'))
-
+bot.command('shipper', => { 
+            
+            
+            
+            
+            })
 
 
 
