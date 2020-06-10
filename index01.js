dfc99efb09952ed63c23651f623d0df89ea7adea
lleellic <66576165+lleellic@users.noreply.github.com>
2020-06-10 05:20:56 +0300

    Update index01.js

--- index01.js
+++ index01.js
@@ -145,17 +145,17 @@ var gamerb = [];
         }]]
     })
 })
-            
+If (callback_data == 'ingameb') {
+  ctx.reply('<a href="tg://user?id='+ ctx.from.id+'">'+ctx.from.first_name+'</a> вступил(а) в игру', {parse_mode : "HTML"})
+gamerb[gamersb] = ctx.from.id;
+gamersb++;
+}            
 } else {
     ctx.reply('Игра уже начата')
   }
 })
 
-If (callback_data == 'ingameb') {
-  ctx.reply('<a href="tg://user?id='+ ctx.from.id+'">'+ctx.from.first_name+'</a> вступил(а) в игру', {parse_mode : "HTML"})
-gamerb[gamersb] = ctx.from.id;
-gamersb++;
-}
+
 bot.hears('startgameb', (ctx) => {
 if (gamersb > 1) {
   gameb(gamersb) 
