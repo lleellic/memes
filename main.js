c9471394772462801b0b0dcc4943f4c61c8bcbb7
lleellic <66576165+lleellic@users.noreply.github.com>
2020-06-10 14:26:53 +0300

    Update main.js

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const bot = new TelegramBot(token, {polling: true});
 var startb = {
   reply_markup: JSON.stringify({
     inline_keyboard: [
-      [{ text: 'Играть😜', callback_data: 'COOMMAND1' },{ text: 'Выйти☹️', callback_data: 'COMMAND2' }],
-      [{ text: 'Старт🤪', callback_data: 'COMMAND3' },{ text: 'Стоп☹️', callback_data: 'COMMAND4' }]
+      [{ text: 'Играть😜', callback_data: 'COOMMAND1' }, { text: 'Выйти☹️', callback_data: 'COMMAND2' }],
+      [{ text: 'Старт🤪', callback_data: 'COMMAND3' }, { text: 'Стоп☹️', callback_data: 'COMMAND4' }]
     ]
   })
 };
@@ -18,16 +18,16 @@ gamebvalue = 1;
 bot.on('callback_query', function (msg) {
             switch(msg.data) {
    case 'COOMMAND1': {
-     bot.sendMessage(msg.chat.id,'<a href="tg://user?id='+ ctx.from.id+'">'+ctx.from.first_name+'</a> вступил(а) в игру', {parse_mode : "HTML"});
+     bot.answerCallbackQuery(msg.chat.id,'<a href="tg://user?id='+ ctx.from.id+'">'+ctx.from.first_name+'</a> вступил(а) в игру', {parse_mode : "HTML"});
    };
    case 'COOMMAND2': {
-     bot.sendMessage(msg.chat.id,'<a href="tg://user?id='+ ctx.from.id+'">'+ctx.from.first_name+'</a> вышел(ла) из игры', {parse_mode : "HTML"})
+     bot.answerCallbackQuery(msg.chat.id,'<a href="tg://user?id='+ ctx.from.id+'">'+ctx.from.first_name+'</a> вышел(ла) из игры', {parse_mode : "HTML"})
    };
    case 'COOMMAND3': {
-     bot.sendMessage(msg.chat.id,'<a href="tg://user?id='+ ctx.from.id+'">'+ctx.from.first_name+'</a> голосуетза старт', {parse_mode : "HTML"})
+     bot.answerCallbackQuery(msg.chat.id,'<a href="tg://user?id='+ ctx.from.id+'">'+ctx.from.first_name+'</a> голосуетза старт', {parse_mode : "HTML"})
    };
    case 'COOMMAND4': {
-     bot.sendMessage(msg.chat.id,'<a href="tg://user?id='+ ctx.from.id+'">'+ctx.from.first_name+'</a> хочет отменить игру', {parse_mode : "HTML"})
+     bot.answerCallbackQuery(msg.chat.id,'<a href="tg://user?id='+ ctx.from.id+'">'+ctx.from.first_name+'</a> хочет отменить игру', {parse_mode : "HTML"})
    }
             }
             });
