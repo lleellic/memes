866f9f403e70439eb6a5016d18a74e9e5de9ca47
lleellic <66576165+lleellic@users.noreply.github.com>
2020-06-10 05:27:16 +0300

    Update index01.js

--- index01.js
+++ index01.js
@@ -149,7 +149,7 @@ If (callback_data == 'ingameb') {
   ctx.reply('<a href="tg://user?id='+ ctx.from.id+'">'+ctx.from.first_name+'</a> вступил(а) в игру', {parse_mode : "HTML"})
 gamerb[gamersb] = ctx.from.id;
 gamersb++;
-}            
+} else {}           
 } else {
     ctx.reply('Игра уже начата')
   }
