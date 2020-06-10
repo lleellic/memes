3322d0608ef9416843503c998928a5bc1554cf05
lleellic <66576165+lleellic@users.noreply.github.com>
2020-06-10 15:45:51 +0300

    Update main.js

--- main.js
+++ main.js
@@ -19,12 +19,11 @@ bot.onText(/Играть в было/i, (msg) => {
  bot.sendMessage(msg.chat.id,'Набор игроков для игры: Было не было', startb);
 gamebvalue = 1;
 })
-bot.on('callback_query', function(msg, match) {
-   
-  if (msg.data=='COOMMAND1') commands1(msg.id, msg.chat.id, msg.from.id, msg.from.first_name)
-  if (msg.data=='COMMAND2') bot.sendMessage(msg.chat.id, 'Вы вышли из игры')
-  if (msg.data=='COMMAND3') bot.sendMessage(msg.chat.id, 'Вы проголосовали за старт')
-  if (msg.data=='COMMAND4')  bot.sendMessage(msg.chat.id, 'Вы проголосовали за закрытие игры')
+bot.on('callback_query', function(msg) {
+  if (msg.data == 'COOMMAND1') commands1(msg.id, msg.chat.id, msg.from.id, msg.from.first_name)
+  if (msg.data == 'COMMAND2') bot.answerCallbackQuery(msg.id, 'Вы вышли из игры', true)
+  if (msg.data == 'COMMAND3') bot.answerCallbackQuery(msg.id, 'Вы проголосовали за старт', true)
+  if (msg.data == 'COMMAND4')  bot.answerCallbackQuery(msg.id, 'Вы проголосовали за закрытие игры', false)
   
  bot.answerCallbackQuery(msg.id, 'Вы выбрали: '+ msg.data, true);
               
