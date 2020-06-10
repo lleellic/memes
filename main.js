ec4ced62061e3fefd1e098f8180c5c56e91df5c7
lleellic <66576165+lleellic@users.noreply.github.com>
2020-06-10 13:23:27 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-.
+const TeleBot = require('telebot');
+
+const bot = new TeleBot('1097903013:AAHjon47mwC7BWolEwQTFZn2lA91iud0ge8')
+
+var startb = {
+  reply_markup: JSON.stringify({
+    inline_keyboard: [
+      [{ text: 'Играть😜', callback_data: 'COOMMAND1' }],
+      [{ text: 'Выйти☹️', callback_data: 'COMMAND2' }]
+     ],
+    [
+     [{ text: 'Старт🤪', callback_data: 'COMMAND3' }],
+     [{ text: 'Стоп☹️', callback_data: 'COMMAND4' }]
+    ]
+  })
+};
+
+bot.on('/startb' (msg) => {
+ bot.sendMessage(msg.from.id,'Набор игроков для игры: Было не было', startb);
+})
