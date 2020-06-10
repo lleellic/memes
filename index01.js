ea675cecc57da25ddd86d9b94c8ef1cfe0eb4a02
lleellic <66576165+lleellic@users.noreply.github.com>
2020-06-10 08:53:08 +0300

    Update index01.js

--- index01.js
+++ index01.js
@@ -2,7 +2,7 @@ const Telegraf = require('telegraf')
 var TelegramBot = require('node-telegram-bot-api');
 const Extra = require('telegraf/extra')
 const fs = require('fs')
-var balancee = require('./balance');
+var balancee = require('./bot/memes/balance');
 var offir = 0;
 
 
