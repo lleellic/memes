96268b438393c8f08293d7037ad44888afda2470
lleellic <66576165+lleellic@users.noreply.github.com>
2020-06-10 13:49:15 +0300

    Update main.js

--- main.js
+++ main.js
@@ -5,12 +5,8 @@ const bot = new TeleBot('1097903013:AAHjon47mwC7BWolEwQTFZn2lA91iud0ge8')
 var startb = {
   reply_markup: JSON.stringify({
     inline_keyboard: [
-      ({ text: 'Играть😜', callback_data: 'COOMMAND1' }),
-      ({ text: 'Выйти☹️', callback_data: 'COMMAND2' })
-     ],
-    [
-     ({ text: 'Старт🤪', callback_data: 'COMMAND3' }),
-     ({ text: 'Стоп☹️', callback_data: 'COMMAND4' })
+      [{ text: 'Играть😜', callback_data: 'COOMMAND1' },{ text: 'Выйти☹️', callback_data: 'COMMAND2' }],
+      [{ text: 'Старт🤪', callback_data: 'COMMAND3' },{ text: 'Стоп☹️', callback_data: 'COMMAND4' }]
     ]
   })
 };
