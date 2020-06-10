2c62ac3b071e3784fa31746f210c9469dc241d0e
lleellic <66576165+lleellic@users.noreply.github.com>
2020-06-10 11:08:38 +0300

    Update index01.js

--- index01.js
+++ index01.js
@@ -135,20 +135,20 @@ let textofg = {
         inline_keyboard: [[
           {
             text: 'Играть!',
-            callback_data: '1_1'
+            callback_data: '1'
           },
           {
             text: 'Выйти!',
-            callback_data: '1_2'
+            callback_data: '2'
           }],
           [
             {
             text: 'Старт!',
-            callback_data: '1_3'
+            callback_data: '3'
           },
           {
             text: 'Стоп!',
-            callback_data: '1_4'
+            callback_data: '4'
           }
           ]]
     })
