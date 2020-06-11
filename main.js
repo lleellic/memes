67775775d69854a3fdc308d4d41e837a639dd228
lleellic <66576165+lleellic@users.noreply.github.com>
2020-06-11 16:12:06 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,292 +1,66 @@
 const TelegramBot = require('node-telegram-bot-api');
 const token = '1097903013:AAHjon47mwC7BWolEwQTFZn2lA91iud0ge8';
 var bot = new TelegramBot(token, {polling: true});
-var offers = 0;
-var offers1 = 0;
-const chatid = -1001241810361;
-var offers2 = 0;
-var i = 0;
-var votestartb = 0;
-var gamersb = 0;
-var gameridb = [];
-var drinkb = [];
-var startb = {
+var chatid = -1001241810361;
+var offirs = 0;
+var s=0;
+var p = 0;
+var gamer = [];
+var inline1 = {
   reply_markup:{
     inline_keyboard: [
-      [{ text: 'Играть😜', callback_data: 'COOMMAND1' }, { text: 'Выйти☹️', callback_data: 'COMMAND2' }]
+      [{ text: 'Играть😋', callback_data: 'COOMMAND1' }, { text: 'Выйти😒', callback_data: 'COMMAND2' }]
     ]
   }
 };
 
-var startb2 = {
+var inline2 = {
   reply_markup:{
     inline_keyboard: [
       [{ text: '🥃', callback_data: 'drink' }, { text: '🥛', callback_data: 'nodrink' }],
-      [{ text: 'Заменить ведущего', callback_data: 'nextw'}]
+[{text:'Сколько участников?', callback_data:'members'}]
     ]
   }
 };
 
-
-
-
-
-
-
-
-
-
-
-
-
-bot.onText(/Играть в было/i, (msg) => {
- if(offers===0){
- bot.sendMessage(msg.chat.id,'Набор игроков для игры: было не было', startb);
- offers = 1;
- } else {
- bot.sendMessage(msg.chat.id,'Игра уже начата');
- }
-  offers1 = 2;
+bot.onText(/играть в было/, (msg) => {
+if (offirs == 0) {
+bot.sendMessage(chatid,'Стартовал набор игроков для игры «Было/не было»', inline1); //тут присвоить значение мессаг айди
+offirs = 1;
+}
+} else {
+bot.answerCallbackQuery(msg.id,'Игра уже начата✋🏼🤚🏼. Ожидайте...', true)
+} 
 })
 
 bot.on('callback_query', function(msg) {
-if (msg.data === 'COOMMAND1') {
- switch (msg.from.id) {
-   case gameridb[0]:bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a>, ты уже в игре', {parse_mode : "HTML"}); break;
-   case gameridb[1]:bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a>, ты уже в игре', {parse_mode : "HTML"}); break;
-   case gameridb[2]:bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a>, ты уже в игре', {parse_mode : "HTML"}); break;
-   case gameridb[3]:bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a>, ты уже в игре', {parse_mode : "HTML"}); break;
-   case gameridb[4]:bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a>, ты уже в игре', {parse_mode : "HTML"}); break;
-   case gameridb[5]:bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a>, ты уже в игре', {parse_mode : "HTML"}); break;
-   case gameridb[6]:bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a>, ты уже в игре', {parse_mode : "HTML"}); break;
-   case gameridb[7]:bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a>, ты уже в игре', {parse_mode : "HTML"}); break;
-   case gameridb[8]:bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a>, ты уже в игре', {parse_mode : "HTML"}); break;
-   case gameridb[9]:bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a>, ты уже в игре', {parse_mode : "HTML"}); break;
-   case gameridb[10]:bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a>, ты уже в игре', {parse_mode : "HTML"}); break;
-   case gameridb[11]:bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a>, ты уже в игре', {parse_mode : "HTML"}); break;
-   default: {
-  gameridb[i] = msg.from.id;
-  gamersb++;
-  bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a> вступил(а) в игру, всего: '+gamersb+' игрок(а/ов)', {parse_mode : "HTML"});
-  bot.answerCallbackQuery(msg.id, 'Вы вошли в игру', true);
-  i++;
-   }; break;
-}}
-  
-if (msg.data === 'COMMAND2') {
-  switch (msg.from.id) {
-   case gameridb[0]:{
-    bot.answerCallbackQuery(msg.id, 'Вы вышли из игры', true);
-    bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a> вышел(ла) из игры', {parse_mode : "HTML"});
-    gameridb[0] = gameridb[gameridb.lenght];
-    gameridb.pop();
-    gamersb--;
-   }; break;
-   case gameridb[1]:{
-    bot.answerCallbackQuery(msg.id, 'Вы вышли из игры', true);
-    bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a> вышел(ла) из игры', {parse_mode : "HTML"});
-    gameridb[1] = gameridb[gameridb.lenght];
-     gameridb.pop();
-    gamersb--;
-   }; break;
-   case gameridb[2]:{
-    bot.answerCallbackQuery(msg.id, 'Вы вышли из игры', true);
-    bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a> вышел(ла) из игры', {parse_mode : "HTML"});
-    gameridb[2] = gameridb[gameridb.lenght];
-     gameridb.pop();
-    gamersb--;
-   }; break;
-   case gameridb[3]:{
-    bot.answerCallbackQuery(msg.id, 'Вы вышли из игры', true);
-    bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a> вышел(ла) из игры', {parse_mode : "HTML"});
-    gameridb[3] = gameridb[gameridb.lenght];
-     gameridb.pop();
-    gamersb--;
-   }; break;
-   case gameridb[4]:{
-    bot.answerCallbackQuery(msg.id, 'Вы вышли из игры', true);
-    bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a> вышел(ла) из игры', {parse_mode : "HTML"});
-    gameridb[4] = gameridb[gameridb.lenght];
-     gameridb.pop();
-    gamersb--;
-   }; break;
-   case gameridb[5]:{
-    bot.answerCallbackQuery(msg.id, 'Вы вышли из игры', true);
-    bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a> вышел(ла) из игры', {parse_mode : "HTML"});
-    gameridb[5] = gameridb[gameridb.lenght];
-     gameridb.pop();
-    gamersb--;
-   }; break;
-   case gameridb[6]:{
-    bot.answerCallbackQuery(msg.id, 'Вы вышли из игры', true);
-    bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a> вышел(ла) из игры', {parse_mode : "HTML"});
-    gameridb[6] = gameridb[gameridb.lenght];
-     gameridb.pop();
-    gamersb--;
-   }; break;
-   case gameridb[7]:{
-    bot.answerCallbackQuery(msg.id, 'Вы вышли из игры', true);
-    bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a> вышел(ла) из игры', {parse_mode : "HTML"});
-    gameridb[7] = gameridb[gameridb.lenght];
-     gameridb.pop();
-    gamersb--;
-   }; break;
-   case gameridb[8]:{
-    bot.answerCallbackQuery(msg.id, 'Вы вышли из игры', true);
-    bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a> вышел(ла) из игры', {parse_mode : "HTML"});
-    gameridb[8] = gameridb[gameridb.lenght];
-     gameridb.pop();
-    gamersb--;
-   }; break;
-   case gameridb[9]:{
-    bot.answerCallbackQuery(msg.id, 'Вы вышли из игры', true);
-    bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a> вышел(ла) из игры', {parse_mode : "HTML"});
-    gameridb[9] = gameridb[gameridb.lenght];
-     gameridb.pop();
-   }; break;
-   case gameridb[10]:{
-    bot.answerCallbackQuery(msg.id, 'Вы вышли из игры', true);
-    bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a> вышел(ла) из игры', {parse_mode : "HTML"});
-    gameridb[10] = gameridb[gameridb.lenght];
-     gameridb.pop();
-    gamersb--;
-   }; break;
-   case gameridb[11]:{
-    bot.answerCallbackQuery(msg.id, 'Вы вышли из игры', true);
-    bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a> вышел(ла) из игры', {parse_mode : "HTML"});
-    gameridb[11] = gameridb[gameridb.lenght];
-     gameridb.pop();
-    gamersb--;
-   }; break;
-    default: bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a>, ты не состоишь в игре', {parse_mode : "HTML"}); break;
-}}
-})
-
-
-bot.on('polling_error', (error) => {
-  console.log(error.code); 
-});
-
-var vv;
-
-bot.onText(/Старт было/i, (msg) => {
-  if (offers1 === 2) {
-bot.sendMessage(chatid, 'Игра начата! Количество игроков: '+gamersb+'.  <a href="tg://user?id='+gameridb+'>Ведущий</a> придумывает вопрос',{parse_mode : "HTML"});
-vv++;
-    bot.sendMessage(gameridb[vv],'Сейчас твоё время задать вопрос. Постарайся придумать его оригинальным. Если придумал, то напиши мне его сюда в виде: Вы...');
-  offers1 = 0;
-  offers3 = 2;
-  }
-})
-
-bot.onText(/вы/i, (msg) => {
-  if (offers3 == 2) {
-  bot.sendMessage(chatid, msg.text+' - если да, то пейте алкоголь (большие шансы опьянеть), если нет, то пейте молокою Надеюсь на вашу честность', startb2);
-  bot.answerCallbackQuery(msg.id, 'Ваш вопрос принят', true);
-  offers3 = 0;
-  }
-})
-
-
-
-bot.on('callback_query', function(msg) {
-if (msg.data === 'drink') {
- switch (msg.from.id) {
-   case gameridb[0]:{
-     bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a> выпил(а) рюмку', {parse_mode : "HTML"})
-     drinkb[0]++;
-     }; break;  
-     case gameridb[1]:{
-     bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a> выпил(а) рюмку', {parse_mode : "HTML"})
-     drinkb[1]++;
-     }; break;  
-     case gameridb[2]:{
-     bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a> выпил(а) рюмку', {parse_mode : "HTML"})
-     drinkb[2]++;
-     }; break;  
-     case gameridb[3]:{
-     bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a> выпил(а) рюмку', {parse_mode : "HTML"})
-     drinkb[3]++;
-     }; break;  
-     case gameridb[4]:{
-     bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a> выпил(а) рюмку', {parse_mode : "HTML"})
-     drinkb[4]++;
-     }; break;  
-     case gameridb[5]:{
-     bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a> выпил(а) рюмку', {parse_mode : "HTML"})
-     drinkb[5]++;
-     }; break;  
-     case gameridb[6]:{
-     bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a> выпил(а) рюмку', {parse_mode : "HTML"})
-     drinkb[6]++;
-     }; break;  
-     case gameridb[7]:{
-     bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a> выпил(а) рюмку', {parse_mode : "HTML"})
-     drinkb[7]++;
-     }; break;  
-     case gameridb[8]:{
-     bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a> выпил(а) рюмку', {parse_mode : "HTML"})
-     drinkb[8]++;
-     }; break;  
-     case gameridb[9]:{
-     bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a> выпил(а) рюмку', {parse_mode : "HTML"})
-     drinkb[9]++;
-     }; break;  
-     case gameridb[10]:{
-     bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a> выпил(а) рюмку', {parse_mode : "HTML"})
-     drinkb[10]++;
-     }; break;  
-     case gameridb[11]:{
-     bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a> выпил(а) рюмку', {parse_mode : "HTML"})
-     drinkb[11]++;
-     }; break;  
-     default: {}; break;
- }} 
-if (msg.data === 'nodrink') {
-  switch (msg.from.id) {
-   case gameridb[0]:bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a> пьёт молоко - ЗОЖник(ца)!', {parse_mode : "HTML"}); break;
-   case gameridb[1]:bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a> пьёт молоко - ЗОЖник(ца)!', {parse_mode : "HTML"}); break;
-   case gameridb[2]:bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a> пьёт молоко - ЗОЖник(ца)!', {parse_mode : "HTML"}); break;
-   case gameridb[3]:bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a> пьёт молоко - ЗОЖник(ца)!', {parse_mode : "HTML"}); break;
-   case gameridb[4]:bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a> пьёт молоко - ЗОЖник(ца)!', {parse_mode : "HTML"}); break;
-   case gameridb[5]:bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a> пьёт молоко - ЗОЖник(ца)!', {parse_mode : "HTML"}); break;
-   case gameridb[6]:bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a> пьёт молоко - ЗОЖник(ца)!', {parse_mode : "HTML"}); break;
-   case gameridb[7]:bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a> пьёт молоко - ЗОЖник(ца)!', {parse_mode : "HTML"}); break;
-   case gameridb[8]:bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a> пьёт молоко - ЗОЖник(ца)!', {parse_mode : "HTML"}); break;
-   case gameridb[9]:bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a> пьёт молоко - ЗОЖник(ца)!', {parse_mode : "HTML"}); break;
-   case gameridb[10]:bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a> пьёт молоко - ЗОЖник(ца)!', {parse_mode : "HTML"}); break;
-   case gameridb[11]:bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a> пьёт молоко - ЗОЖник(ца)!', {parse_mode : "HTML"}); break;
-   default: {}; break;
-}}
-if (msg.data === 'nextw') {
-   bot.answerCallbackQuery(msg.id, 'В разработке...', true)
+if (msg.data === 'COOMMAND1') { 
+for (s=0; s<gamer.lenght;s++){
+ if (gamer.id[s]==msg.from.id){
+p++;
+} 
+if (p==0){
+gamer[i] =  {firstname=msg.from.first_name, id=msg.from.id, volume=0};
+i++;
+} else {
+bot.answerCallbackQuery(msg.id,'Вы уже в игре', true)
+}
+}
+}
+if (msg.data === 'COOMMAND2'){
+p=-1;
+for (i=0;i<gamer.lenght;i++){
+if (gamer.id[i]==msg.from.id){ 
+p==i;
+}
+if (p>-1) {
+shift.gamer[i]
+} else {
+bot.answerCallbackQuery(msg.id,'Вы итак не в игре😳',true)
+}
+}
+}
+if (msg.data === 'members'){
+bot.answerCallbackQuery(msg.id,gamer.lenght+' участник(ов)',true)
 }
 })
-
-bot.onText(/[0-9]/, (msg) => {
-  if (msg.from.id == 684519513) bot.deleteMessage(msg.chat.id, msg.text)
-})
-
-bot.onText(/Вопрос далее/i, (msg) => {
- for (var y; y<11; y++) {
-   if (drink[y] == 5) {
-     bot.sendMessage(msg.chat.id, 'Количество игроков: '+gamersb+'. <a href="tg://user?id='+gameridb[y]+'>Ты</a> опьянел (вылетел из игры)',{parse_mode : "HTML"});
-     drink[y] = 0;
-     gameridb[y] = gameridb[gameridb.lenght];
-     gameridb.pop();
-     gamersb--;
-   }}
-  if (gamersb > 1){
- bot.sendMessage(msg.chat.id, 'Количество игроков: '+gamersb+'. <a href="tg://user?id=684519813>Ведущий</a> придумывает вопрос',{parse_mode : "HTML"});
- bot.sendMessage(gameridb[vv],'Сейчас твоё время задать вопрос. Постарайся придумать его оригинальным. Если придумал, то напиши мне его сюда в виде: Вы...');
-  vv++;
-  } else if (gamersb == 1){
-    bot.sendMessage(chatid, '<a href="tg://user?id='+gameridb[0]+'">Победитель</a> этой игры!', {parse_mode : "HTML"});
-  } else {
-    bot.sendMessage(chatid, 'Никто не выйграл😢');
-  }
-})
-
-
-
-
