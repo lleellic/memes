b69f3cb993c5dddc070638c345248ae588fdaf55
lleellic <66576165+lleellic@users.noreply.github.com>
2020-06-11 04:06:19 +0300

    Update main.js

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ if (msg.data === 'COOMMAND1'){
   bot.answerCallbackQuery(msg.id, 'Вы вошли в игру', true);
   bot.sendMessage(msg.message.chat.id, '<a href="tg://user?id='+ msg.from.id+'">'+msg.from.first_name+'</a> вступил(а) в игру', {parse_mode : "HTML"});
  // gamersb++;
-  bot.edited_message_text(infochatidb, infostartb, 'Набор игроков для игры: было не было, всего: '+gamersb+' игрок(а/ов)', startb)
+//  bot.edited_message_text(infochatidb, infostartb, 'Набор игроков для игры: было не было, всего: '+gamersb+' игрок(а/ов)', startb)
 //  gsmeridb[i] = msg.from.id;
  // i++;
 }
