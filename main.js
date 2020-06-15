7dfa8fab52b2a2a2e6c90898c463acdee98cdd70
lleellic <66576165+lleellic@users.noreply.github.com>
2020-06-15 16:07:34 +0800

    Update main.js

--- main.js
+++ main.js
@@ -39,6 +39,7 @@ var inline2 = {
 bot.onText(/играть в было/i, (msg) => {
 chatid = msg.from.chat_id;
 if (offirs === 0) {
+console.log(msg);
  bot.sendMessage(chatid,'Стартовал набор игроков для игры «Было/не было»', inline1); 
 offirs = 1;
 } else {
