9e0c3022fc1584c3ac6942305acbc64c7d8c73f9
lleellic <66576165+lleellic@users.noreply.github.com>
2020-07-13 03:42:13 +0300

    Update fame.js

--- fame.js
+++ fame.js
@@ -2,7 +2,7 @@ TelegramBot = require('node-telegram-bot-api');
 const token = '1097903013:AAHjon47mwC7BWolEwQTFZn2lA91iud0ge8'; 
 var bot = new TelegramBot(token, {polling: true});
 const sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('./bot/memes/balance.db', sqlite3.OPEN_READWRITE, (err) => {
+var db = new sqlite3.Database('./root/bot/memes/balance.db', sqlite3.OPEN_READWRITE, (err) => {
   if (err) {
     console.error(err.message);
   }
