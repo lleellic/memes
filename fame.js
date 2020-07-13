13fc7f1a38efcd5ff1e39cdcb5ec46d94a94c2bc
lleellic <66576165+lleellic@users.noreply.github.com>
2020-07-13 08:55:41 +0300

    Update fame.js

--- fame.js
+++ fame.js
@@ -42,6 +42,34 @@ b[0] = ' 2️⃣ ';
 c[0] = ' 3️⃣ ';
 d[0] = ' 4️⃣ ';
 
+let db = new sqlite3.Database(':memory:', (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+});
+
+db.serialize(() => {
+  // Queries scheduled here will be serialized.
+  db.run('CREATE TABLE greetings(message text)')
+    .run(`INSERT INTO greetings(message) VALUES('Hi'), ('Hello'), ('Welcome')`)
+    .each(`SELECT message FROM greetings`, (err, row) => {
+      if (err){
+        throw err;
+      }
+      console.log(row.message);
+    });
+});
+
+// close the database connection
+db.close((err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+});
+
+
+
+
 
 var inline1 = {
   reply_markup:{
