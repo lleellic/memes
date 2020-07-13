56ee77e2ef105551735091f3a9721c5f4aeaef3d
lleellic <66576165+lleellic@users.noreply.github.com>
2020-07-13 09:26:49 +0300

    Update fame.js

--- fame.js
+++ fame.js
@@ -44,7 +44,8 @@ const db = new sqlite3.Database('./mytest.db', (err) => {
 
 bot.onText(/конфеты/i, (msg) => {
  db.serialize(() => {
-   db.get('SELECT bal FROM bal WHERE id = ' + msg.from.id, (err, row) => {
+   db. .run(`INSERT INTO bal(id, bal) VALUES(' +msg.from.id+ ', 50))
+     .get('SELECT bal FROM bal WHERE id = ' + msg.from.id, (err, row) => {
      console.log('2');
      if (err){
         throw err;
