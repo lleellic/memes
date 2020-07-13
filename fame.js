3158afa86a63054c1b3fe3b02f0830decf298f3f
lleellic <66576165+lleellic@users.noreply.github.com>
2020-07-13 03:43:56 +0300

    Update fame.js

--- fame.js
+++ fame.js
@@ -5,6 +5,8 @@ const sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('./root/bot/memes/balance.db', sqlite3.OPEN_READWRITE, (err) => {
   if (err) {
     console.error(err.message);
+    f = ('CREATE TABLE balance(id int, bal int DEFAULT 0)';
+    db.run(f);
   }
   console.log('Соединение прошло успешно');
 });
