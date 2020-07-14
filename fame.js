cc866523b587e3a478043b85a765b1e674a022e4
lleellic <66576165+lleellic@users.noreply.github.com>
2020-07-14 11:17:28 +0300

    Update fame.js

--- fame.js
+++ fame.js
@@ -106,6 +106,7 @@ bot.onText(/^бонус (.+) (.+)/i, (msg, match) => {
     psum = match[2];
     db.get('SELECT bal FROM ba3 WHERE id ='+pid, (err, row) => {
          if (!row) db.run('INSERT INTO ba3(id, bal) VALUES('+pid+', 0)')
+      resul = row;
       console.log(resul);
          });
            db.run('UPDATE ba3 SET bal = '+(psum)+' WHERE id = '+pid);
