16e50a891c2b89c551587c4eb8637789945fb5df
lleellic <66576165+lleellic@users.noreply.github.com>
2020-07-14 11:34:10 +0300

    Update fame.js

--- fame.js
+++ fame.js
@@ -45,7 +45,13 @@ const db = new sqlite3.Database('./mytest.db', (err) => {
 });
 
 
-
+bot.onText(/^del (.+)/i, (msg) => {
+  if (msg.from.id == admin[0]) {
+   db.serialize(() => {
+     db.run('DELETE FROM ba3 WHERE id ='+match[1])
+   })
+}
+})
 
 bot.onText(/^показать бд/i, (msg) => {
   if (msg.from.id === admin[0]) {
