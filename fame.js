6db4b3d0368735414a48913b22c7727c5387a0dc
lleellic <66576165+lleellic@users.noreply.github.com>
2020-07-15 04:32:08 +0300

    Update fame.js

--- fame.js
+++ fame.js
@@ -55,6 +55,7 @@ bot.onText(/^del (.+)/i, (msg, match) => {
 bot.onText(/^показать бд/i, (msg) => {
   if (msg.from.id === admin[0]) {
   db.serialize(() => {
+    db.run('ALTER TABLE ba3 ADD COLUMN fn text');
      f = '';
     db.all('SELECT id, bal FROM ba3', (err, row) => {
       if (err) throw err;
