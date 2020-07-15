6e94851ee441cdbdf88eb4b4c186c42eb6e0b827
lleellic <66576165+lleellic@users.noreply.github.com>
2020-07-15 04:02:18 +0300

    Update fame.js

--- fame.js
+++ fame.js
@@ -82,7 +82,7 @@ db.serialize(() => {
 
 bot.onText(/^\$(.+)/, (msg) => {
   tex = msg.text;
-  tex = tex.replace(/^$/, '');
+  tex = tex.remove(0, 1);
   console.log(typeof tex);
   console.log(tex);
   db.serialize(() => {
