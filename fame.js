bcd335e7261adbb095f6d0ecb65bfa8d2da6ac00
lleellic <66576165+lleellic@users.noreply.github.com>
2020-07-16 06:15:40 +0300

    Update fame.js

--- fame.js
+++ fame.js
@@ -215,7 +215,7 @@ switch(gamersb) {
   case 2: s = 1; can = 2; inl = 2; break;
   case 3: s = 1; can = 5; inl = 3; break;
   case 4: s = 1; can = 4; inl = 3; break;
-  case 5: s = 1; can = 3; inl = 3; break;
+  case 5: s = 1; can = 4; inl = 3; break;
   case 6: s = 2; can = 9; inl = 4; break;
   case 7: s = 2; can = 8; inl = 4; break;
   case 8: s = 2; can = 7; inl = 4; break;
@@ -541,8 +541,7 @@ if (msg.data > 0) {
   reply_markup:{
     inline_keyboard: [
       [{text: pole[1], callback_data:1}, {text: pole[2], callback_data:2}],
-      [{text: pole[3], callback_data:3}, {text: pole[4], callback_data:4}],
-      [{text: 'Готово', callback_data:'ready'}]
+      [{text: pole[3], callback_data:3}, {text: pole[4], callback_data:4}]
     ]
   }
 };
@@ -551,10 +550,9 @@ i3 = {
   parse_mode: "HTML",
   reply_markup:{
     inline_keyboard: [
-      [{text: pole[1], callback_data:1}, {text: pole[2], callback_data:3}, {text: pole[3], callback_data:3}],
+      [{text: pole[1], callback_data:1}, {text: pole[2], callback_data:2}, {text: pole[3], callback_data:3}],
       [{text: pole[4], callback_data:4}, {text: pole[5], callback_data:5}, {text: pole[6], callback_data:6}],
-      [{text: pole[7], callback_data:7}, {text: pole[8], callback_data:8}, {text: pole[9], callback_data:9}],
-      [{text: 'Готово', callback_data:'ready'}]
+      [{text: pole[7], callback_data:7}, {text: pole[8], callback_data:8}, {text: pole[9], callback_data:9}]
     ]
   }
 };
@@ -566,8 +564,7 @@ i4 = {
       [{text: pole[1], callback_data:1},   {text: pole[2], callback_data:2},   {text: pole[3], callback_data:3},   {text: pole[4],  callback_data:4} ],
       [{text: pole[5], callback_data:5},   {text: pole[6], callback_data:6},   {text: pole[7], callback_data:7},   {text: pole[8],  callback_data:8} ],
       [{text: pole[9], callback_data:9},   {text: pole[10], callback_data:10}, {text: pole[11], callback_data:11}, {text: pole[12], callback_data:12}],
-      [{text: pole[13], callback_data:13}, {text: pole[14], callback_data:14}, {text: pole[15], callback_data:15}, {text: pole[16], callback_data:16}],
-      [{text: 'Готово', callback_data:'ready'}]
+      [{text: pole[13], callback_data:13}, {text: pole[14], callback_data:14}, {text: pole[15], callback_data:15}, {text: pole[16], callback_data:16}]
     ]
   }
 };
@@ -580,8 +577,7 @@ i5 = {
       [{text: pole[6], callback_data:6},     {text: pole[7], callback_data:7},     {text: pole[8], callback_data:8},     {text: pole[9],  callback_data:9},   {text: pole[10], callback_data:10}],
       [{text: pole[11], callback_data:11},   {text: pole[12], callback_data:12},   {text: pole[12], callback_data:13},   {text: pole[14],  callback_data:14}, {text: pole[15], callback_data:15}],
       [{text: pole[16], callback_data:16},   {text: pole[17], callback_data:17},   {text: pole[18], callback_data:18},   {text: pole[19],  callback_data:19}, {text: pole[20], callback_data:20}],
-      [{text: pole[21], callback_data:21},   {text: pole[22], callback_data:22},   {text: pole[23], callback_data:23},   {text: pole[24],  callback_data:24}, {text: pole[25], callback_data:25}],
-      [{text: 'Готово', callback_data:'ready'}]
+      [{text: pole[21], callback_data:21},   {text: pole[22], callback_data:22},   {text: pole[23], callback_data:23},   {text: pole[24],  callback_data:24}, {text: pole[25], callback_data:25}]
     ]
   }
 }; 
@@ -592,7 +588,6 @@ i5 = {
             case 5: opt = i5; break;
             case 6: opt = 0; break;  
            }
-        console.log('2.3')
         bot.editMessageText('Игра стартовала!\n\nШансов на поиск ('+can+')\n\nИскатели:\n'+f+'\nОстальные прячущиеся - '+(hide.length-found)+'\n\nВнимание!\nКоманда: искать!', opt);
       } else {
         can--;
