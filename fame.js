fad9746033196fedd450f9f3fb6680fcd77286d1
lleellic <66576165+lleellic@users.noreply.github.com>
2020-07-16 11:21:05 +0300

    Update fame.js

--- fame.js
+++ fame.js
@@ -541,38 +541,7 @@ if (msg.data > 0) {
   }
   } else {
     if (seek.includes(msg.from.id)) {
-      if (can === 0) {
-          h = 0;
-          f2 = '';
-          f = '';
-          for (i = 0; i < placeval.length; i++) {
-            if (placeval[i] === 1) {
-              for (i2 = 0; i2 < place.length; i2++) {
-                if (place[i2] === i) {
-                  h++;
-                  f2 += h +'. '+hidefn[i2]+' +2 🍬\n';
-                  db.run('UPDATE ba3 SET bal = bal + 2 WHERE id = '+hide[i2]);
-                }
-              }
-            }
-          }
-
-          s = Math.floor(((gamersb - h) * 2)/s);
-          for (i = 0; i < seek.length; i++) {
-            f += (i+1)+'. '+seekfn[i]+' +'+s+' 🍬\n';
-            db.run('UPDATE ba3 SET bal = bal + '+s+' WHERE id = '+seek[i]);
-          }
-          bot.deleteMessage(chatt, em);
-          bot.sendMessage(chatt,'Игра окончена!\n\Искатели:\n'+f+'\nКого не нашли:\n\n'+f2+'\n\nПоздравляем!', {parse_mode: "HTML"}); 
-          offirs = 0;
-          gamer.length = 0;
-          user.length = 0;
-          hide.length = 0;
-          hidefn.length = 0;
-          seek.length = 0;
-          seekfn.length = 0;
-          gamersb = 0;
-      } else {
+      if (can !== 0) {
         console.log('clickcan');
       if (placeval[msg.data] === 2) {
         bot.answerCallbackQuery(msg.id,'Сюда уже тыкали', false);
@@ -747,7 +716,7 @@ console.log('includes');
             db.run('UPDATE ba3 SET bal = bal + '+s+' WHERE id = '+seek[i]);
 }
           bot.deleteMessage(chatt, em);
-          bot.sendMessage(chatt, 'Игра окончена!\n\Победители:\n'+f+'\nВсех нашли\n\nПоздравляем искателей!', {parse_mode: "HTML"}); 
+          bot.sendMessage(chatt, 'Игра окончена!\n\Победители:\n'+f+'\nВсех нашли!\n\nПоздравляем искателей!', {parse_mode: "HTML"}); 
           offirs = 0;
           gamer.length = 0;
           user.length = 0;
