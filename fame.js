4d84ab33f4071417a5fb320b3792538e51b906f5
lleellic <66576165+lleellic@users.noreply.github.com>
2020-07-16 08:10:48 +0300

    Update fame.js

--- fame.js
+++ fame.js
@@ -656,7 +656,93 @@ i5 = {
         opt.message_id = em;
         bot.editMessageText('Игра стартовала!\n\nШансов на поиск ('+can+')\n\nИскатели:\n'+f+'\nОстальные прячущиеся - '+(hide.length-found)+'\n\nВнимание!\nКоманда: искать!', opt);
       }
-        if ((can === 0) || (!placeval.includes(1))) {
+        if (!placeval.includes(1)) {
+          can = -1;
+          h = 0;
+          f2 = '';
+          f = '';
+          for (i = 0; i < placeval.length; i++) {
+            if (placeval[i] === 1) {
+              for (i2 = 0; i2 < place.length; i++) {
+                if (place[i2] === i) {
+                  h++;
+                  f2 += h+'. '+hidefn[i2]+' +2 🍬\n';
+                  pole[i] = '  '
+                  db.run('UPDATE ba3 SET bal = bal + 2 WHERE id = '+hide[i2]);
+                }
+              }
+            }
+          }
+          s = Math.floor(((gamersb - h) * 2)/s);
+          for (i = 0; i < seek.length; i++) {
+            f += (i+1)+'. '+seekfn[i]+' +'+s+' 🍬\n';
+            db.run('UPDATE ba3 SET bal = bal + '+s+' WHERE id = '+seek[i]);
+          }
+              i2 = {
+  parse_mode: "HTML",
+  reply_markup:{
+    inline_keyboard: [
+      [{text: pole[1], callback_data:1}, {text: pole[2], callback_data:2}],
+      [{text: pole[3], callback_data:3}, {text: pole[4], callback_data:4}]
+    ]
+  }
+};
+
+i3 = {
+  parse_mode: "HTML",
+  reply_markup:{
+    inline_keyboard: [
+      [{text: pole[1], callback_data:1}, {text: pole[2], callback_data:3}, {text: pole[3], callback_data:3}],
+      [{text: pole[4], callback_data:4}, {text: pole[5], callback_data:5}, {text: pole[6], callback_data:6}],
+      [{text: pole[7], callback_data:7}, {text: pole[8], callback_data:8}, {text: pole[9], callback_data:9}]
+    ]
+  }
+};
+
+i4 = {  
+  parse_mode: "HTML",
+  reply_markup:{
+    inline_keyboard: [
+      [{text: pole[1], callback_data:1},   {text: pole[2], callback_data:2},   {text: pole[3], callback_data:3},   {text: pole[4],  callback_data:4} ],
+      [{text: pole[5], callback_data:5},   {text: pole[6], callback_data:6},   {text: pole[7], callback_data:7},   {text: pole[8],  callback_data:8} ],
+      [{text: pole[9], callback_data:9},   {text: pole[10], callback_data:10}, {text: pole[11], callback_data:11}, {text: pole[12], callback_data:12}],
+      [{text: pole[13], callback_data:13}, {text: pole[14], callback_data:14}, {text: pole[15], callback_data:15}, {text: pole[16], callback_data:16}]
+    ]
+  }
+};
+
+i5 = {
+  parse_mode: "HTML",
+  reply_markup:{
+    inline_keyboard: [
+      [{text: pole[1], callback_data:1},     {text: pole[2], callback_data:2},     {text: pole[3], callback_data:3},     {text: pole[4],  callback_data:4},   {text: pole[5], callback_data:5}  ],
+      [{text: pole[6], callback_data:6},     {text: pole[7], callback_data:7},     {text: pole[8], callback_data:8},     {text: pole[9],  callback_data:9},   {text: pole[10], callback_data:10}],
+      [{text: pole[11], callback_data:11},   {text: pole[12], callback_data:12},   {text: pole[12], callback_data:13},   {text: pole[14],  callback_data:14}, {text: pole[15], callback_data:15}],
+      [{text: pole[16], callback_data:16},   {text: pole[17], callback_data:17},   {text: pole[18], callback_data:18},   {text: pole[19],  callback_data:19}, {text: pole[20], callback_data:20}],
+      [{text: pole[21], callback_data:21},   {text: pole[22], callback_data:22},   {text: pole[23], callback_data:23},   {text: pole[24],  callback_data:24}, {text: pole[25], callback_data:25}]
+    ]
+  }
+}; 
+          switch(inl) {
+            case 2: opt = i2; break;
+            case 3: opt = i3; break;
+            case 4: opt = i4; break;
+            case 5: opt = i5; break;
+            case 6: opt = 0; break;  
+           };
+          opt.chat_id = chatt;
+          opt.message_id = em;
+          bot.editMessageText('Игра окончена!\n\Победители:\n'+f+'\nКого не нашли:\n\n'+f2+'\n\nПоздравляем!', opt); 
+          offirs = 0;
+          gamer.length = 0;
+          user.length = 0;
+          hide.length = 0;
+          hidefn.length = 0;
+          seek.length = 0;
+          seekfn.length = 0;
+          gamersb = 0;
+        }
+        if (can === 0) {
           h = 0;
           f2 = '';
           f = '';
